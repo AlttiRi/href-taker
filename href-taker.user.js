54a8207e4294e78b3255394947ce894489ab952b
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2023-03-04 09:45:49 +0300

    Bump

--- href-taker.user.js
+++ href-taker.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        HrefTaker
-// @version     0.2.2-2023.03.04
+// @version     0.2.3-2023.03.04
 // @namespace   gh.alttiri
 // @description URL grabber popup
 // @license     GPL-3.0
