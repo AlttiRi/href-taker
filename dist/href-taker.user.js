22e8fb25244ca9defbbb8665141dfdfbd3dfe0d7
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2023-04-21 02:46:37 +0300

    Support Middle Mouse Button "click" events on `a` tags

--- dist/href-taker.user.js
+++ dist/href-taker.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        HrefTaker
-// @version     0.10.1-2023.4.20-e5cc
+// @version     0.10.2-2023.4.20-8cf3
 // @namespace   gh.alttiri
 // @description URL grabber popup
 // @license     GPL-3.0
@@ -516,12 +516,34 @@ function getListHelper(container, settings) {
     const contentElem = container.querySelector(`#result-list`);
     const mainHost = url => new URL(url).hostname.split(".").slice(-2).join(".");
 
+    contentElem.addEventListener("click", onClickMarkUrlAsClicked);
+    contentElem.addEventListener("contextmenu", onAltContextMenuUnmarkClickedUrl);
+    contentElem.addEventListener("pointerup", onMMBPointerUpMarkUrlAsClicked);
+
     const clickedUrls = new Set();
     /** @param {MouseEvent} event */
     function onClickMarkUrlAsClicked(event) {
-        if (!event.target.classList.contains("visible")) {
+        if (!event.target.closest(".url-item-link")) {
             return;
         }
+        markUrlAsClicked(event);
+    }
+
+    /** @param {PointerEvent} event */
+    function onMMBPointerUpMarkUrlAsClicked(event) {
+        const MIDDLE_BUTTON = 1;
+        if (event.button !== MIDDLE_BUTTON) {
+            return;
+        }
+        const activeElement = container.getRootNode().activeElement;
+        if (activeElement !== event.target.closest(".url-item-link")) {
+            return;
+        }
+        markUrlAsClicked(event);
+    }
+
+    /** @param {MouseEvent|PointerEvent} event */
+    function markUrlAsClicked(event) {
         const urlItem = event.target.closest(".url-item");
         urlItem.classList.add("clicked");
         const url = urlItem.querySelector("a").href;
@@ -531,7 +553,6 @@ function getListHelper(container, settings) {
             dupLink.closest(".url-item").classList.add("clicked");
         }
     }
-    contentElem.addEventListener("click", onClickMarkUrlAsClicked);
 
     /** @param {MouseEvent} event */
     function onAltContextMenuUnmarkClickedUrl(event) {
@@ -555,7 +576,6 @@ function getListHelper(container, settings) {
             dupLink.closest(".url-item").classList.remove("clicked");
         }
     }
-    contentElem.addEventListener("contextmenu", onAltContextMenuUnmarkClickedUrl);
 
     function urlToHtml(url) {
         let {prefix = "", main} = url.match(/(?<prefix>^https?:\/\/(www\.)?)?(?<main>.+)/i).groups;
@@ -592,6 +612,7 @@ function getListHelper(container, settings) {
             const title = `title="RMB click to temporary toggle Unsearchable option"`;
             headerElem.innerHTML = `<span class="header-content" ${title}>Result list (${urls.length})</span> <span class="urls-hash">#${hexes.toUpperCase()}</span>`;
 
+            const anchorAttr = `class="url-item-link" target="_blank" rel="noreferrer noopener"`;
             let resultHtml = "";
             let prev = urls[0];
             for (const url of urls) {
@@ -607,7 +628,7 @@ function getListHelper(container, settings) {
                     }
                 }
                 const clicked = clickedUrls.has(url) ? " clicked" : "";
-                const html = `<div class="url-item${clicked}"><a href="${url}" target="_blank" rel="noreferrer noopener">${linkHtml}</a></div>`;
+                const html = `<div class="url-item${clicked}"><a href="${url}" ${anchorAttr}>${linkHtml}</a></div>`;
                 resultHtml += html;
             }
             contentElem.insertAdjacentHTML("beforeend", resultHtml);
